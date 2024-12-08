0b29262f8002b0b562aa81390b3dff15dd1fe981
Nyamato023 <nyamatoelton@gmail.com>
2024-12-08 19:11:32 +0300

    Revert "gallery routes complete"
    This reverts commit 5d34e8c34e9e7ece5066a06c17b24ebdfdaade48.

--- resources/views/components/alert.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['type'])
-
-<div class="px-4 py-3 rounded border-l-4" :class="{
-    'bg-green-100 border-green-400 text-green-700': '{{ $type }}' === 'success',
-    'bg-red-100 border-red-400 text-red-700': '{{ $type }}' === 'error',
-}">
-    {{ $slot }}
-</div>
